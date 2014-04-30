9c10ced540768e855ef11d31956e07642ec8f019
Mark Allen <maallen87@gmail.com>
2014-04-30 22:10:26 +0100

    Started location aware implementation

--- src/com/grimewad/smart_currency_converter/MainActivity.java
+++ src/com/grimewad/smart_currency_converter/MainActivity.java
@@ -5,16 +5,28 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.widget.Toast;
 
 import org.json.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener {
 	
 	private Map<String, String> countriesCurrenciesMap = new HashMap<String, String>();
+	
+	private LocationManager locationManager;
+	
+	private String locationProvider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,12 @@ public class MainActivity extends Activity {
 			Toast.makeText(this, "Error loading Currency Codes", Toast.LENGTH_LONG).show();
 			this.finish();
 		}
+		
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		locationProvider = locationManager.getBestProvider(criteria, false);
+		Location location = locationManager.getLastKnownLocation(locationProvider);
+	
 	}
 
 	@Override
@@ -65,6 +83,49 @@ public class MainActivity extends Activity {
 			String currencyCode = country.getString("currency");
 			countriesCurrenciesMap.put(countryCode, currencyCode);
 		}
+	}
+	
+	  public static class ErrorDialogFragment extends DialogFragment {
+	        // Global field to contain the error dialog
+	        private Dialog mDialog;
+	        // Default constructor. Sets the dialog field to null
+	        public ErrorDialogFragment() {
+	            super();
+	            mDialog = null;
+	        }
+	        // Set the dialog to display
+	        public void setDialog(Dialog dialog) {
+	            mDialog = dialog;
+	        }
+	        // Return a Dialog to the DialogFragment.
+	        @Override
+	        public Dialog onCreateDialog(Bundle savedInstanceState) {
+	            return mDialog;
+	        }
+	    }
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
